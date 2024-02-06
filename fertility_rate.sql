20ef9e1194f21cb4b2131dbd01da8c7c1ba63b7a
Wojciech <57535089+WojciechAdamiak@users.noreply.github.com>
2024-02-06 19:20:20 +0100

    fertility_rate
    fertility rate, european countries

---
+++ fertility_rate.sql
@@ -0,0 +1,47 @@
+INSERT INTO fertility_rate (Country,Fertility_rate,`Ranking fertility rate`) VALUES
+	 ('Monaco',2.1,1),
+	 ('France',1.8,2),
+	 ('Moldova',1.8,2),
+	 ('Ireland',1.8,2),
+	 ('Estonia',1.7,5),
+	 ('Sweden',1.7,5),
+	 ('Romania',1.7,5),
+	 ('Montenegro',1.7,5),
+	 ('Iceland',1.7,5),
+	 ('Czech Republic',1.7,5);
+INSERT INTO fertility_rate (Country,Fertility_rate,`Ranking fertility rate`) VALUES
+	 ('Denmark',1.7,5),
+	 ('Latvia',1.6,12),
+	 ('United Kingdom',1.6,12),
+	 ('Lithuania',1.6,12),
+	 ('Belgium',1.6,12),
+	 ('Bulgaria',1.6,12),
+	 ('Netherlands',1.6,12),
+	 ('Slovenia',1.6,12),
+	 ('Slovakia',1.6,12),
+	 ('Hungary',1.6,12);
+INSERT INTO fertility_rate (Country,Fertility_rate,`Ranking fertility rate`) VALUES
+	 ('Austria',1.5,21),
+	 ('Belarus',1.5,21),
+	 ('Norway',1.5,21),
+	 ('Poland',1.5,21),
+	 ('Liechtenstein',1.5,21),
+	 ('Serbia',1.5,21),
+	 ('Germany',1.5,21),
+	 ('Switzerland',1.5,21),
+	 ('Croatia',1.4,29),
+	 ('Finland',1.4,29);
+INSERT INTO fertility_rate (Country,Fertility_rate,`Ranking fertility rate`) VALUES
+	 ('Luxembourg',1.4,29),
+	 ('North Macedonia',1.4,29),
+	 ('Greece',1.4,29),
+	 ('Albania',1.4,29),
+	 ('Portugal',1.4,29),
+	 ('Bosnia and Herzegovina',1.3,36),
+	 ('Italy',1.3,36),
+	 ('Spain',1.3,36),
+	 ('Ukraine',1.3,36),
+	 ('Malta',1.2,40);
+INSERT INTO fertility_rate (Country,Fertility_rate,`Ranking fertility rate`) VALUES
+	 ('Andorra',1.1,41),
+	 ('San Marino',1.1,41);
